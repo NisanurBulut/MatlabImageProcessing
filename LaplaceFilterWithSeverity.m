9c515dfa5469fc9c461fa58f0b7e29d65d9e9c6c
NisanurBulut <nisanurrunasin@gmail.com>
2018-12-10 17:36:32 +0300

    Update LaplaceFilterWithSeverity.m

--- LaplaceFilterWithSeverity.m
+++ LaplaceFilterWithSeverity.m
@@ -1,13 +1,14 @@
 clc;
 close all;
-AT=imread('R2256x256.jpg');
-AT=rgb2gray(AT);
-AT=im2double(AT);
-[r, c]=size(AT);
-A1=im2double(AT);
-filter=[1 1 1;1 -8 1; 1 1 1];
-A2=A1;
-
+AT=imread('R2256x256.jpg');%oku
+AT=rgb2gray(AT);%gri seviyeye indirge
+AT=im2double(AT);%double formata getir
+[r, c]=size(AT);%boyutlarını al
+filter=[1 1 1;1 -8 1; 1 1 1];%maske tanımı
+A1=AT;
+A2=AT;
+figure,imshow(AT);%orjinal resmi göster
+degree=0;%Laplace katsayısı
 for i=2:r-1
     for j=2:c-1
         sum=0;
@@ -17,44 +18,25 @@ for i=2:r-1
             row=row+1;
             col=1;
             for l=j-1:j+1                
-                sum = sum+A1(k,l)*filter(row,col);               
+                sum = sum+A1(k,l)*filter(row,col);    %laplace sonucunu al           
                 col=col+1;
             end
         end
       A2(i,j)=sum;      
     end
 end
-sharpenedA3 = imsubtract(AT, A2);%Imge-Lalpace uygulanmış imge
-degreeLaplace=0;
-Ad0=A2;
-for i=2:r-1
-    for j=2:c-1
-        Ad0(r,j)=A2(r,c)*degreeLaplace;
-    end
-end
-sharpenedA0 = imsubtract(AT, Ad0);%Imge-Lalpace uygulanmış imge
-Ad0=uint8(Ad0);%laplace uygulanmış resim
-
-degreeLaplace=0.25;
-Ad1=A2;
-for i=2:r-1
-    for j=2:c-1
-        Ad1(r,j)=A2(r,c)*degreeLaplace;
-    end
-end
-sharpenedA1 = imsubtract(AT, Ad1);%Imge-Lalpace uygulanmış imge
-Ad1=uint8(Ad1);%laplace uygulanmış resim
-
-degreeLaplace=0.75;
-Ad2=A2;
-for i=2:r-1
-    for j=2:c-1
-        Ad2(r,j)=A2(r,c)*degreeLaplace;
-    end
-end
-sharpenedA2 = imsubtract(AT, Ad2);%Imge-Lalpace uygulanmış imge
-Ad2=uint8(Ad2);%laplace uygulanmış resim
-subplot(2,2,1);imshow(sharpenedA0);title('Laplace Katsayı=0')
-subplot(2,2,2); imshow(sharpenedA1);title('Laplace Katsayı=0.25')
-subplot(2,2,3);imshow(sharpenedA2);title('Laplace Katsayı=0.75')
-subplot(2,2,4);imshow(sharpenedA3);title('Laplace Katsayı=1')
+C0 = A2.*degree;
+sharpened0=imsubtract(AT,C0);
+degree=0.25;
+C025 = A2.*degree;
+sharpened075=imsubtract(AT,C025);
+degree=0.75;
+C075 = A2.*degree;
+sharpened025=imsubtract(AT,C075);
+degree=1;
+C1 = A2.*degree;
+sharpened1=imsubtract(AT,C1);
+subplot(2,2,1);imshow(sharpened0);title('Laplace Katsayı=0')
+subplot(2,2,2); imshow(sharpened025);title('Laplace Katsayı=0.25')
+subplot(2,2,3);imshow(sharpened075);title('Laplace Katsayı=0.75')
+subplot(2,2,4);imshow(sharpened1);title('Laplace Katsayı=1')
